5e6ff5cc2a5abe476bc4dcf00fb4b46a2b3fce54
Unknown <sevenflash@yandex.ru>
2018-10-27 22:52:03 +0300

    remove cors headers

--- src/eos.js
+++ src/eos.js
@@ -7,11 +7,6 @@ const context = new Router()
 
 const app = Express()
   .use(require('body-parser').json())
-  .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
   .use(context)
   .listen(process.env.PORT)
 
